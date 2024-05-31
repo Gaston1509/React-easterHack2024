ca2e7ccb3d9297ea1e625a64df47668c5f84a65d
Gaston Meza <gaston1509@gmail.com>
2024-05-31 13:49:56 -0300

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import ComponentePrimero from "./ComponentePrimero";
+
+function App() {
+  return (
+    <>
+      <div className="contenedor">
+        <h1>Hola desde App</h1>
+        <p>------</p>
+        <ComponentePrimero />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/ComponentePrimero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ComponentePrimero = () => {
+  return (
+    <div>
+      <h2>Desde el primer Componente</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam
+        impedit totam!
+      </p>
+    </div>
+  );
+};
+
+export default ComponentePrimero;
